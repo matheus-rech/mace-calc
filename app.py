de39a56bae3636699d5a78a146749dbe9bc0a798
Matheus Rech <mmrech@users.noreply.huggingface.co>
2023-10-14 14:06:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -68,11 +68,28 @@ features_array = np.array(list(features.values())).reshape(1, -1)
 
 # Define the predict function
 def predict(features_array):
-    # Make predictions using the model
     prediction = model.predict(features_array)
     return prediction
 
+# Make prediction and display result
 if st.button('Predict'):
-    # Get the prediction
     prediction = predict(features_array)
-    st.write(f"Prediction: {prediction[0]}")
+    if prediction[0] == 1:
+        st.write("High risk of MACE")
+    elif prediction[0] == 0:
+        st.write("Low risk of MACE")
+    else:
+        st.write("Unexpected prediction value: please check the model.")
+
+# Add disclaimer
+disclaimer_text = """
+#### Disclaimer
+
+*This tool is designed for general educational purposes only and is not intended in any way to substitute for professional medical advice, consultation, diagnosis, or treatment. Any analysis, report, or information contained in or produced by this tool is intended to serve as a supplement to, and not a substitute for the knowledge, expertise, skill and judgment of health care professionals. In no event shall this tool under this Agreement, be considered to be in any form, medical care, treatment, or therapy for patients or users of this tool.*
+
+*This tool's services are provided 'as is'. These services provide no warranties, express or implied and shall not be liable for any direct, consequential, lost profits, or other damages incurred by the user of this information tool.*
+
+*The default values included in the web application are placeholders. Clinicians can modify the inputs as per the clinical characteristics of individual patients to examine the impact on survival prediction in real-time. Clinicians should be aware that the algorithms require complete information for the factors included in the interface.*
+"""
+
+st.markdown(disclaimer_text, unsafe_allow_html=True)
